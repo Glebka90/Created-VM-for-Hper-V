79954950d8331ef53d3365e5bbbe76a3b7de95a5
Glebka90 <gllebka@DC-01.>
2025-08-13 14:27:59 +0300

    Добавил список свичей

--- vars/Network-VM.ps1
+++ vars/Network-VM.ps1
@@ -1,2 +1,4 @@
 #Get-VMSwitch
+Write-Host "Список коммутаторов:"
+Get-VMSwitch | ForEach-Object { Write-Host " - $_.Name" }
 $switchName = Read-Host "Укажите комутатор (Список комутаторов Get-VMSwitch)"
